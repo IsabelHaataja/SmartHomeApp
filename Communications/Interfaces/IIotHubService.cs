df08ee5e9f41822359e6b61480107f4e71a44aaf
Isabel Haataja <isabelhaataja@gmail.com>
2024-11-27 17:13:30 +0100

    Added interfaces and removed unnecessary classes

---
+++ Communications/Interfaces/IIotHubService.cs
@@ -0,0 +1,8 @@
+﻿namespace Communications.Interfaces
+{
+    public interface IIotHubService
+    {
+        Task InvokeDeviceMethodAsync(string deviceId, string methodName);
+        Task SendCloudToDeviceMessageAsync(string deviceId, string message);
+    }
+}
