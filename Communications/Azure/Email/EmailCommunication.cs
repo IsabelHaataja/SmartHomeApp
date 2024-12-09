5712ddcdfce7ff22ddded17980cb386f93baf21f
Isabel Haataja <isabelhaataja@gmail.com>
2024-12-09 17:29:35 +0100

    Added try catch blocks

--- Communications/Azure/Email/EmailCommunication.cs
+++ Communications/Azure/Email/EmailCommunication.cs
@@ -2,27 +2,20 @@
 using Azure.Communication.Email;
 using Azure;
 using System.Diagnostics;
-using Microsoft.Extensions.Logging;
+
 
 namespace Communications.Azure.Email;
 
 public class EmailCommunication
 {
-    private readonly ILogger _logger;
     private readonly string _senderAddress;
     private readonly EmailClient _client;
 
     public EmailCommunication(string connectionString = "endpoint=https://cs-f9d56587.europe.communication.azure.com/;accesskey=CQkhvdwAOXkS4ij430nGjgQV6AFPny0k6t6ckEttxCxkqAhh8OciJQQJ99AIACULyCp3CzXbAAAAAZCSfJdY", string senderAddress = "DoNotReply@8abdee84-8838-400b-b09a-0263d8d33c27.azurecomm.net")
     {
-        try
-        {
+
             _client = new EmailClient(connectionString);
             _senderAddress = senderAddress;
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine("Error in EmailCommunication constructor", ex);
-        }
     }
 
     public void Send(string toAddress, string subject, string body, string plainTextBody)
